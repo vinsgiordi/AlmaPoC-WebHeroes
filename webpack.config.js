f43d8e5ab8520bd6064b4a16ba86ede8d9c8d454
v.giordano <v.giordano@almaviva.it>
2024-05-24 15:40:21 +0200

    Updated the gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@
 
 const path = require('path')
 const autoprefixer = require('autoprefixer')
-//const webpack = require('webpack');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
